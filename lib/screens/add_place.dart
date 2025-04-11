91d64480e68a4121258ea3e618b7f0119f5cf00c
JagdishSingh01 <jatinpawar354@gmail.com>
2025-04-11 22:06:13 +0530

    added the maps api for getting current location

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,5 +1,7 @@
+import 'package:favourite_places/models/place.dart';
 import 'package:favourite_places/providers/user_places.dart';
 import 'package:favourite_places/widgets/image_input.dart';
+import 'package:favourite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:io';
@@ -14,14 +16,17 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _selectedImage;
+  PlaceLocation? _selectedLocation;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
-    if (enteredTitle.isEmpty || _selectedImage ==null) {
+    if (enteredTitle.isEmpty || _selectedImage == null ||_selectedLocation==null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle,_selectedImage!);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, _selectedImage!,_selectedLocation!);
     Navigator.of(context).pop();
   }
 
@@ -52,6 +57,14 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             ImageInput(
               onPickImage: (image) {
                 _selectedImage = image;
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            LocationInput(
+              onSelectLocation: (location) {
+                _selectedLocation = location;
               },
             ),
             const SizedBox(
